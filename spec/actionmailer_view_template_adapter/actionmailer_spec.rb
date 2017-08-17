e6a1875c10b02eb3c660257f761d72f529c79f83
Asher King Abramson <akingabramson@appacademy.io>
2017-08-17 14:50:58 -0700

    Tests are running

--- spec/actionmailer_view_template_adapter/actionmailer_spec.rb
+++ spec/actionmailer_view_template_adapter/actionmailer_spec.rb
@@ -1,9 +1,9 @@
-require 'spec_helper'
+require 'rails_helper'
 
 describe Dummy::Application do
   let(:config) { described_class.config }
 
-  it "has default view_adapter config" do
+  it "has an action_mailer.view_adapter attribute" do
     expect(config.action_mailer.view_adapter).to eq("local")
   end
 end
